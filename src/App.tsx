ea0489d855c7f7b376a37c27ea02ce9df0532ab1
Dagim <dagimgetaW27@gmail.com>
2025-09-17 17:54:23 +0300

    chore: setup initial react/vite project with typescript with styling taiwlindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default App;
